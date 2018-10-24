396cc8406597d9d40d10a0f199e86c62ee6839a1
Li Linfang <nonellf@gmail.com>
2018-10-24 14:25:32 +0800

    package init

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+        name='cnyear',
+        version = '0.0.1',
+        description = 'A tool for conversion between Chinese year and CE.',
+        author = 'Nemo',
+        author_email = 'nonellf@gmail.com',
+        url = 'https://github.com/nemo-nullius/cnyear',
+        license = 'GPLv3',
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Topic :: Database',
+            'Programming Language :: Python :: 3.7',
+            ],
+        keywords = 'Chinese regnal year CE conversion',
+        packages = find_packages(exclude=['tests']),
+        )
+
